42d2e9fb3cadefcfb8f32b20d0f288d0ea72cb95
Mathias Panzenböck <grosser.meister.morti@gmx.net>
2021-08-30 00:13:51 +0200

    debug--

--- examples/parse_json.c
+++ examples/parse_json.c
@@ -16,28 +16,6 @@ size_t strlen16(const char16_t* strarg) {
 int main(int argc, char *argv[]) {
     int status = 0;
 
-    const char16_t *keys[] = {
-        u"a",
-        u"b",
-        u"foo",
-        u"bar",
-        u"baz",
-        NULL
-    };
-
-    for (const char16_t **k1 = keys; *k1; ++ k1) {
-        for (const char16_t **k2 = keys; *k2; ++ k2) {
-            size_t l1 = strlen16(*k1);
-            size_t l2 = strlen16(*k2);
-            int cmp = jsonlogic_utf16_compare(*k1, l1, *k2, l2);
-            printf("cmp(\"");
-            jsonlogic_print_utf16(stdout, *k1, l1);
-            printf("\", \"");
-            jsonlogic_print_utf16(stdout, *k2, l2);
-            printf("\"): %d\n", cmp);
-        }
-    }
-
     for (int argind = 1; argind < argc; ++ argind) {
         JsonLogic_LineInfo info  = JSONLOGIC_LINEINFO_INIT;
         JsonLogic_Handle   value = jsonlogic_parse(argv[argind], &info);
