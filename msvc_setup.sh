69a2d3d1ea5ef4661fa7e571aadcae6fb12025f5
Mathias Panzenböck <grosser.meister.morti@gmx.net>
2022-02-08 03:45:49 +0100

    msvc install dir can now contain spaces

--- msvc_setup.sh
+++ msvc_setup.sh
@@ -5,15 +5,6 @@ set -e
 SELF=$(readlink -f "$0")
 DIR=$(dirname "$SELF")
 
-function count_args () {
-	echo $#
-}
-
-if [[ $(count_args $DIR) -gt 1 ]]; then
-	echo "MSVC installation directory may not contain spaces: '$DIR'">&2
-	exit 1
-fi
-
 if [[ -e "$DIR/msvc" ]]; then
 	echo "$DIR/msvc already exists.">&2
 	echo "If you want to do a clean install of the MSVC toolchain delete this directory.">&2
